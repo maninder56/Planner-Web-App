cb4ee1ed888dc149671c1198c80516230bd717ef
Maninder Singh <maninder123450@gmail.com>
2026-01-17 13:11:58 +0000

    Added styles for button

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import Button from "@/Components/Buttons/button";
@@ -12,15 +14,42 @@ export default function Home() {
           <h1>Planner</h1>
         </div>
         <div>
-          <Link href={'/login'} className="button transparent" >Log in</Link>
+          <Link href={'/login'} className='button transparent'>Log in</Link>
         </div>
         <div>
-          <Link href={'/signup'} className="button red">Sign up</Link>
+          <Link href={'/signup'} className='button red'>Sign up</Link>
         </div>
       </header>
-
       <main className={styles.main}>
-        
+        <section>
+          <div>
+            <div>
+              <p>Have a bunch of to-dos and can&apos;t decide where to start?</p>
+              <p>No worries — we have a solution. Start planning your project with our planner.</p>
+            </div>
+            <div>
+              <Link href={'/signup'} className='button red'>Sign up</Link>
+            </div>
+            <div>
+              <p>don&apos;t want to create an account?, then just create an guest account.</p>
+            </div>
+            <div>
+              <Button name='Guest' color='red' onClick={() => {}} />
+            </div>
+          </div>
+          <div>
+            <div>
+              <Image src={'/no-image.svg'}  alt='Dashboard image' width={100} height={100} loading='eager' />
+            </div>
+            <div>
+              <p>
+                This is a demo web application, Please do not enter any private or sensitive information. 
+                You can explore the app using a Guest Account with auto-generated sample data.
+              </p>
+              <p>Please note that guest accounts are intended for temporary use and are not retained for extended periods.</p>
+            </div>
+          </div>
+        </section>
       </main>
       <footer>
 
