0f36e272235a468b4b32270e122fa41672370fdb
Maninder Singh <maninder123450@gmail.com>
2026-01-20 20:22:44 +0000

    Added third section and footer

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -58,11 +58,44 @@ export default function Home() {
         <section className={styles.secondSection}>
           <header>What can I do?</header>
           <AppFeaturesTabbedNavigation />
-          <AppFeaturesTabbedNavigation />
+        </section>
+        <section className={styles.thirdSection}>
+          <div>
+            <div className={styles.contentWrapper}>
+              <header>Start Your planning</header>
+              <div className={styles.contentGrid}>
+                <div className={styles.signupLinkContainer}>
+                  <Link href={'/signup'} className='button red'>Sign up</Link>
+                </div>
+                <p>Signing up will alow you to invite other members, so you can work together on a project</p>
+                <p>Or create a guest account to try the planner without needing an account. But you won&apos;t be able to invite or collaborate with other members</p>
+                <div className={styles.guestButtonContainer}>
+                  <Button name='Guest' color='black' onClick={() => {}} />
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
       </main>
       <footer>
-
+        <div>
+          <div className={styles.footerWrapper}>
+            <div className={styles.appHeading}>
+              <Image src={'/Site-Logo.svg'} alt='App logo' width={50} height={50} loading='eager'/>
+              <h1>Planner</h1>  
+            </div>
+            <div>
+              <div>
+                <Link href={'/login'} className='button'>About Planner</Link>
+              </div>
+            </div>
+            <div>
+              <div>
+                <Link href={'/login'} className='button'>Contact</Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </footer>
     </div>
   );
