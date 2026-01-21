6e87359c3f7ce5c23fd989ba3f94f9b434f64e90
Maninder Singh <maninder123450@gmail.com>
2026-01-21 11:55:23 +0000

    Added layout for login and sign up pages

--- front-end/src/app/(login-signup)/layout.tsx
+++ front-end/src/app/(login-signup)/layout.tsx
@@ -1,13 +1,24 @@
+
+import styles from '@/app/(login-signup)/layout.module.css'; 
+import Image from 'next/image';
+
 export default function Layout({
     children
 }: {
     children: React.ReactNode
 }) {
     return (
-        <div>
+        <div className={styles.page}>
             <main>
-                <p>lay out of both log in and sign up</p>
-                {children}
+                <section>
+                    <div className={styles.appLogo}>
+                        <Image src={'/Site-Logo.svg'} alt='App logo' width={50} height={50} loading='eager'/>
+                        <h1>Planner</h1>
+                    </div>
+                    <div>
+                        {children}
+                    </div>
+                </section>
             </main>
         </div>
     ); 
