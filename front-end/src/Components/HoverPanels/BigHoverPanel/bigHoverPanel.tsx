12e36e7d5231c639249f165c71ffd77da3a9d436
Maninder Singh <maninder123450@gmail.com>
2026-02-03 11:58:35 +0000

    Added animations to hover panel

--- front-end/src/Components/HoverPanels/BigHoverPanel/bigHoverPanel.tsx
+++ front-end/src/Components/HoverPanels/BigHoverPanel/bigHoverPanel.tsx
@@ -1,6 +1,8 @@
+'use client'
 
 import CloseButton from '@/Components/Buttons/closeButton';
 import styles from './bigHoverPanel.module.css'; 
+import { useState } from 'react';
 
 export default function BigHoverPanel({
     title, 
@@ -11,13 +13,25 @@ export default function BigHoverPanel({
     onCloseClick: () => void; 
     children: React.ReactNode; 
 }) {
+    const [closing, setClosing] = useState(false); 
+
+    function handleClose() {
+        setClosing(true); 
+        setTimeout(onCloseClick, 200); // match animation duration
+    }
+
     return (
-        <div className={styles.wrapper} onClick={e => { e.stopPropagation(); onCloseClick(); }}>
-            <div className={styles.panel} onClick={e => { e.stopPropagation(); }}>
+        <div className={[styles.wrapper, closing ? styles.fadeOut : ''].join(' ')} 
+            onClick={e => { 
+                e.stopPropagation(); 
+                handleClose(); 
+            }}>
+            <div className={[styles.panel, closing ? styles.slideOut: ''].join(' ')} 
+                onClick={e => { e.stopPropagation(); }}>
                 <div className={styles.titleAndCloseButton}>
                     <header>{title}</header>
                     <div className={styles.closeButton} onClick={e => {e.stopPropagation();}}>
-                        <CloseButton onClick={onCloseClick} />
+                        <CloseButton onClick={handleClose} />
                     </div>
                 </div>
                 <div>
