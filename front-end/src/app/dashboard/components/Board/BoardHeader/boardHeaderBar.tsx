8a8b8e56d6750ed6422a14b84d33e63065ad7e85
Maninder Singh <maninder123450@gmail.com>
2026-02-05 12:50:18 +0000

    Added favorite button

--- front-end/src/app/dashboard/components/Board/BoardHeader/boardHeaderBar.tsx
+++ front-end/src/app/dashboard/components/Board/BoardHeader/boardHeaderBar.tsx
@@ -1,12 +1,16 @@
 
 import styles from './boardHeaderBar.module.css'; 
 import BoardNameInput from './BoardNameInput/boardNameInput';
+import FavoriteBoardButton from './FavoriteBoardButton/favoriteBoardButton';
 
 export default function BoardHeaderBar() {
     return (
         <div className={styles.wrapper}>
             <div>
                 <BoardNameInput initialName='My First board' />
+            </div>
+            <div>
+                <FavoriteBoardButton initialState={false} />
             </div>
         </div>
     ); 
