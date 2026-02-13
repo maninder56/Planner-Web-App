53e19415e8f124e4ddbab58f6c220350295d8306
Maninder Singh <maninder123450@gmail.com>
2026-02-13 11:01:53 +0000

    Working dnd with two lists

--- front-end/src/app/practice/page.tsx
+++ front-end/src/app/practice/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { closestCenter, DndContext, DragEndEvent, KeyboardSensor, PointerSensor, UniqueIdentifier, useSensor, useSensors } from '@dnd-kit/core';
-import { useState } from 'react';
+import { act, useState } from 'react';
 import Droppable from './components/droppable';
 import Draggable from './components/draggable';
 import { arrayMove, SortableContext, sortableKeyboardCoordinates, verticalListSortingStrategy } from '@dnd-kit/sortable';
@@ -23,60 +23,75 @@ export default function Practice() {
         const {active, over} = event; 
 
         if (active.id !== over?.id) {
+
+            const activeItemInListA = typeof(listA.find(i => i === active.id)) !== 'undefined'; 
+            const overItemInListA = typeof(listA.find(i => i === over?.id)) !== 'undefined'; 
+
+            const activeItemInListB = typeof(listB.find(i => i === active.id)) !== 'undefined'; 
+            const overItemInListB = typeof(listB.find(i => i === over?.id)) !== 'undefined'; 
+        
+            const listBEmpty = listB.length === 0; 
+            const listAEmpty = listA.length === 0; 
+
             // if they are in same container
-            if (listA.find(i => i === active.id) && listA.find(i => i === over?.id)) {
+            if (activeItemInListA && overItemInListA) { // if they are in same comtainer for A
                 setListA((listA) => {
                     const newIndex = listA.indexOf(over?.id as number); 
                     const oldIndex = listA.indexOf(active.id as number); 
 
                     return arrayMove(listA, oldIndex, newIndex); 
                 })
-            } else if (listB.find(i => i === active.id) && listB.find(i => i === over?.id)) {
+            } else if (activeItemInListB && overItemInListB) { // if they are in same comtainer for B
                 setListB((listB) => {
                     const newIndex = listB.indexOf(over?.id as number); 
                     const oldIndex = listB.indexOf(active.id as number); 
 
                     return arrayMove(listB, oldIndex, newIndex); 
                 })
-            } else if (listA.find(i => i === over?.id) && listB.find(i => i === active.id)) { // if element moves from B -> A
-                setListB(listB.filter(i => i !== active.id)); 
+            } else if (overItemInListA && activeItemInListB && active.id !== 0) { // if element moves from B -> A
+                const newListB = listB.filter(i => i !== active.id)
+
+                if (newListB.length === 0) {
+                    setListB([0]); 
+                } else {
+                    setListB(newListB); 
+                }
+
+                const newListA = listA.filter(i => i !== 0); 
+
                 setListA(() => {
-                    const list = [...listA, active.id]; 
-                    const newIndex = listA.indexOf(over?.id as number); 
-                    const oldIndex = listA.length; 
+                    const list = [...newListA, active.id]; 
+                    const newIndex = newListA.indexOf(over?.id as number); 
+                    const oldIndex = newListA.length; 
 
                     return arrayMove(list, oldIndex, newIndex) as number[]; 
                 })
-            } else if (listB.find(i => i == over?.id) && listA.find(i => i === active.id)) { // if element moves from A -> B
-                setListA(listA.filter(i => i !== active.id)); 
+            } else if (overItemInListB && activeItemInListA && active.id !== 0) { // if element moves from A -> B
+
+                const newListA = listA.filter(i => i !== active.id); 
+
+                if (newListA.length === 0) {
+                    setListA([0]); 
+                } else {
+                    setListA(newListA); 
+                }
+
+                const newListB = listB.filter(i => i !== 0); 
+
                 setListB(() => {
-                    const list = [...listB, active.id]; 
-                    const newIndex = listB.indexOf(over?.id as number); 
-                    const oldIndex = listB.length; 
+                    const list = [...newListB, active.id]; 
+                    const newIndex = newListB.indexOf(over?.id as number); 
+                    const oldIndex = newListB.length; 
 
                     return arrayMove(list, oldIndex, newIndex) as number[]; 
                 })
-            } else if (listB.length === 0 && listA.find(i => i === active.id)) { // if list B is empty and element moves from A -> B
-                setListA(listA.filter(i => i !== active.id)); 
-                setListB(() => {
-                    const list = [...listB, active.id]; 
-                    const newIndex = 0; 
-                    const oldIndex = list.length; 
-
-                    return arrayMove(list, oldIndex, newIndex) as number[]; 
-                })
-            }
+            } 
         }
     }
 
     function handleDragOver(event: DragEndEvent) {
         const {active, over} = event; 
 
-        // if (active.id !== over?.id) {
-        //     if (listA.find(i => i !== active.id) && listB.find(i => i !== over?.id)) {
-        //         setListB(listB.filter(i => i !== active.id)); 
-        //     }
-        // }
 
         console.log(`A: ${active.id}, O: ${over?.id}`); 
         console.log(`ListA: ${listA}, ListB: ${listB}`); 
