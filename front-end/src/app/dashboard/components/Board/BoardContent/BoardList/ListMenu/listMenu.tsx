66262325d3db77f70598910eeec05a0763e04480
Maninder Singh <maninder123450@gmail.com>
2026-02-23 13:00:31 +0000

    Initial files for list menu button

---
+++ front-end/src/app/dashboard/components/Board/BoardContent/BoardList/ListMenu/listMenu.tsx
@@ -0,0 +1,11 @@
+
+
+import styles from './listMenu.module.css'; 
+
+export default function ListMenu() {
+    return (
+        <div className={styles.wrapper}>
+
+        </div>
+    ); 
+}
--- front-end/src/app/dashboard/components/Board/BoardContent/BoardList/boardList.tsx
+++ front-end/src/app/dashboard/components/Board/BoardContent/BoardList/boardList.tsx
@@ -3,6 +3,7 @@ import { useSortable, UseSortableInput} from '@dnd-kit/react/sortable';
 import {CollisionPriority} from '@dnd-kit/abstract';
 import styles from './boardList.module.css'; 
 import { ListId, useBoardStore } from '@/app/dashboard/Store/boardStore';
+import Image from 'next/image';
 
 export default function BoardList({
     children, 
@@ -13,7 +14,7 @@ export default function BoardList({
     listId: ListId; 
     index: number; 
 }) {
-    const {ref} = useSortable({
+    const {ref, isDragging} = useSortable({
         id: listId,  
         index,
         type: 'boardList',
@@ -23,17 +24,20 @@ export default function BoardList({
     const listDetails = useBoardStore((state) => state.lists[listId]); 
 
     return (
-        <div className={styles.wrapper} ref={ref}>
-            <div>
+        <div className={[styles.wrapper, isDragging ? styles.dragging : ''].join(' ')} ref={ref}>
+            <div className={styles.header}>
                 <header>{listDetails.title}</header>
                 {/* Create list menu which only opens if list id and panel are matched */}
                 <button>list Menu</button>
             </div>
-            <div>
+            <div className={styles.cards}>
                 {children}
             </div>
-            <div>
-                <button>Add card button</button>
+            <div className={styles.addNewCardContainer}>
+                <button>
+                    <Image src={'./plusSign.svg'} alt='plus sign' width={10} height={10} />
+                    <span>Add card button</span>
+                </button>
             </div>
         </div>
     ); 
