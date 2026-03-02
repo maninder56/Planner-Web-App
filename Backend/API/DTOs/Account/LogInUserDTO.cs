59a875bedb8721ccffa3ad2a24132474763afc24
Maninder Singh <maninder123450@gmail.com>
2026-03-02 10:48:51 +0000

    Added validation for login object

--- Backend/API/DTOs/Account/LogInUserDTO.cs
+++ Backend/API/DTOs/Account/LogInUserDTO.cs
@@ -6,8 +6,10 @@ public class LogInUserDTO
 {
     [Required]
     [EmailAddress]
+    [StringLength(200)]
     public required string Email { get; set; }
 
     [Required]
+    [StringLength(100)]
     public required string Password { get; set; }
 }
