8e7280ec88a756cc1b6a3af3aa842b9807149174
Maninder Singh <maninder123450@gmail.com>
2026-03-07 21:33:59 +0000

    Added custom exception and initial file for exception handler

---
+++ Backend/API/Exceptions/BadRequestException.cs
@@ -0,0 +1,6 @@
+﻿using System.Net;
+
+namespace API.Exceptions; 
+
+public sealed class BadRequestException(string message) 
+    : AppException(message, HttpStatusCode.BadRequest); 
---
+++ Backend/API/Handler/GlobalExceptionHandler.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace API.Handler;
+
+public sealed class GlobalExceptionHandler(
+    ILogger<GlobalExceptionHandler> logger,
+    IProblemDetailsService problemDetailsService) : IExceptionHandler
+{
+    public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
