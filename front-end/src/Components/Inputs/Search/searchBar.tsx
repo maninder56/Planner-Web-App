ec2067d4c4f86f9f076733e7a655d7d877a6348e
Maninder Singh <maninder123450@gmail.com>
2026-03-23 11:33:39 +0000

    Fixed typos in svg element

--- front-end/src/Components/Inputs/Search/searchBar.tsx
+++ front-end/src/Components/Inputs/Search/searchBar.tsx
@@ -20,8 +20,8 @@ export default function SearchBar({
     return (
         <div className={styles.wrapper}>
             <svg fill="none" viewBox="4.75 4.25 15.5 15.5">
-                <path clip-rule="evenodd" d="M5.5 10.766a5.765 5.765 0 1 1 11.53 0 5.765 5.765 0 0 1-11.53 0" 
-                    stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path clipRule="evenodd" d="M5.5 10.766a5.765 5.765 0 1 1 11.53 0 5.765 5.765 0 0 1-11.53 0" 
+                    strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                 <path d="M17.029 16.53 19.5 19" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
             <input 
