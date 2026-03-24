77a07bfd1d98a8ff4e77a3864451ffb08c06826f
Maninder Singh <maninder123450@gmail.com>
2026-03-24 13:00:30 +0000

    New board api request function added

--- front-end/src/app/dashboard/Services/boardService.tsx
+++ front-end/src/app/dashboard/Services/boardService.tsx
@@ -1,7 +1,7 @@
-import { ApiErrorFromStatusCode, ApiRequestFailed, ApiRequestSuccessfull } from "@/Utilities/ApiUtilities";
-import z from "zod";
-import { BoardSchema, BoardDataFromAPI } from "../Types/boardTypes";
-import { ApiFetchRequest } from "@/Services/ApiRequest";
+import { ApiErrorFromStatusCode, ApiRequestFailed, ApiRequestSuccessfull } from '@/Utilities/ApiUtilities';
+import z from 'zod';
+import { BoardSchema, BoardDataFromAPI, BoardColour } from '../Types/boardTypes';
+import { ApiFetchRequest } from '@/Services/ApiRequest';
 
 const boardRoute = '/boards'; 
 
@@ -10,6 +10,45 @@ export async function LastUsedBoardRequest() {
     const request: RequestInit = {
         method: 'GET', 
         credentials: 'include',
+    }; 
+
+    try {
+        const response = await ApiFetchRequest(subUrl, request); 
+
+        if (response.ok) {
+            const data = await response.json(); 
+            const validData = BoardSchema.safeParse(data); 
+
+            if (validData.success) {
+                return ApiRequestSuccessfull(validData.data); 
+            } else {
+                console.error('Invalid data recieved from API'); 
+                return ApiRequestFailed('DataValidationFailed'); 
+            }
+
+        } else {
+            return ApiErrorFromStatusCode(response.status); 
+        }
+    } catch(error) {
+        console.error('Error: ', error); 
+        return ApiRequestFailed('FetchRequestFailed'); 
+    }
+}
+
+
+
+export async function CreateNewBoardRequest(name: string, colour: BoardColour) {
+    const subUrl = boardRoute; 
+    const request: RequestInit = {
+        headers: {
+            'Content-Type': 'text/json',
+        }, 
+        method: 'POST', 
+        credentials: 'include',
+        body: JSON.stringify({
+            Name: name,
+            BackgroundColour: colour,
+        }),
     }; 
 
     try {
