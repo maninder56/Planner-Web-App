01c9777202c79eccb5109fa98cae56598a09b6b2
Maninder Singh <maninder123450@gmail.com>
2026-04-30 17:11:11 +0100

    Added styles for hidden cards

--- front-end/src/app/dashboard/components/Board/BoardContent/BoardCard/boardCard.tsx
+++ front-end/src/app/dashboard/components/Board/BoardContent/BoardCard/boardCard.tsx
@@ -35,26 +35,6 @@ export default function BoardCard({
 }) {
     const viewOnly = userRole === 'Viewer'; 
 
-    const {ref, handleRef, isDragging} = useSortable({
-        id: cardId, 
-        index, 
-        type: 'boardCard', 
-        accept: 'boardCard',
-        collisionPriority: CollisionPriority.Normal,
-        collisionDetector: closestCenter, 
-        // modifiers: [RestrictToWindow], 
-        group: parentListId, 
-        data: {
-            parentListId,
-            index,
-        }, 
-        disabled: viewOnly, 
-        transition: {
-            duration: 0, 
-            idle: false,
-        }
-    }); 
-
     const boardId = useBoardStore((state) => state.currentBoardData?.id); 
     const parentListIdAsNumber = ConvertListIdToNumeric(parentListId); 
     const cardDetails = useBoardStore((state) => state.cards[cardId]);
@@ -74,6 +54,27 @@ export default function BoardCard({
     const cardDescription = cardDetails.description.length > 100 ?  
         `${cardDetails.description.slice(0, 100)}...` : 
         cardDetails.description; 
+    const bigCard = cardDescription.length > 100; 
+
+    const {ref, handleRef, isDragging} = useSortable({
+        id: cardId, 
+        index, 
+        type: 'boardCard', 
+        accept: 'boardCard',
+        collisionPriority: CollisionPriority.Normal,
+        collisionDetector: closestCenter, 
+        // modifiers: [RestrictToWindow], 
+        group: parentListId, 
+        data: {
+            parentListId,
+            index,
+        }, 
+        disabled: viewOnly, 
+        transition: {
+            duration: 0, 
+            idle: false,
+        }
+    }); 
 
     async function handleDoneOnCard(cardId: CardId, isDone: boolean) {
         if (boardId === undefined || parentListIdAsNumber === -1) {
@@ -103,8 +104,8 @@ export default function BoardCard({
 
     if (isCardHidden) {
         return (
-            <div className={`${styles.wrapper} ${styles.hidden}`} ref={ref}>
-                <svg className='fadeIn' xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
+            <div className={[styles.wrapper, styles.hidden].join(' ')} ref={ref}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
                     <path d="M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7M2 4.27l2.28 2.28.46.46A11.8 11.8 0 0 0 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2m4.31-.78 3.15 3.15.02-.16c0-1.66-1.34-3-3-3z"/>
                 </svg>
             </div>
@@ -115,7 +116,8 @@ export default function BoardCard({
     return (
         <div className={[styles.wrapper, 
             IsCardDone ? styles.taskDone : '', 
-            isDragging? styles.isDragging : ''].join(' ')} ref={ref} 
+            isDragging ? styles.isDragging : '', 
+            bigCard ? styles.bigCard : ''].join(' ')} ref={ref} 
             onClick={(e) => {
                 e.stopPropagation(); 
                 setActivePanel('cardDetailsPanel');
