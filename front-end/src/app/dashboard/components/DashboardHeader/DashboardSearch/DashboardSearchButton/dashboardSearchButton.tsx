18f28570e48bb2a3e4e70feaf5bcbc44b3a99b0f
Maninder Singh <maninder123450@gmail.com>
2026-05-05 09:51:26 +0100

    feature to open card added from search results for mobiel version

--- front-end/src/app/dashboard/components/DashboardHeader/DashboardSearch/DashboardSearchButton/dashboardSearchButton.tsx
+++ front-end/src/app/dashboard/components/DashboardHeader/DashboardSearch/DashboardSearchButton/dashboardSearchButton.tsx
@@ -13,17 +13,26 @@ import { useUserStore } from '@/Store/userStore';
 import { CardSearchResult } from '@/app/dashboard/Types/cardTypes';
 import { ApiRequestWithRefreshTokenAttemptAndData } from '@/Services/ApiRequest';
 import { SearchCardByKeywordRequest } from '@/app/dashboard/Services/cardService';
+import { GetBoardRequest, UpdateLastUsedBoardRequest } from '@/app/dashboard/Services/boardService';
+import { NormaliseBoardData } from '@/app/dashboard/Utilities/boardData';
 
 export default function DashboardSearchButton() {
     const isPanelOpen = useBoardUIStore((state) => state.activePanel === 'searchButtonPanel'); 
     const setActivePanel = useBoardUIStore((state) => state.setActivePanel); 
     const isBoardLoading = useBoardStore((state) => state.isBoardLoading); 
+    const [searchInput, setSearchInput] = useState(''); 
 
     const [loading, setLoading] = useState(true); 
     const [searchResults, setSearchResults] = useState<CardSearchResult | undefined>(); 
+    
     const setSessionExpired = useUserStore((state) => state.setSessionExpired); 
+    const boardDetails = useBoardStore((state) => state.currentBoardData); 
+    const setCardDetailsPanelData = useBoardUIStore((state) => state.setCardDetailsPanelData); 
+    const setBoardLoading = useBoardStore((state) => state.setBoardLoading); 
+    const hydrateBoard = useBoardStore((state) => state.hydrateBoard); 
+    const setLastUsedBoardExists = useBoardStore((state) => state.setLastUsedBoardExists); 
 
-    const [searchInput, setSearchInput] = useState(''); 
+    const [errorMessage, setErrorMessage] = useState(''); 
 
     const debounceTimmerRef = useRef<NodeJS.Timeout | null>(null); 
     const inputRef = useRef<HTMLInputElement>(null); 
@@ -51,14 +60,73 @@ export default function DashboardSearchButton() {
         const request = await ApiRequestWithRefreshTokenAttemptAndData(SearchCardByKeywordRequest, search); 
 
         if (request.ok && request.data !== undefined) {
+            setErrorMessage(''); 
             setSearchResults(request.data); 
         } else if (!request.ok && request.error === 'Unauthorized') {
             setActivePanel('none'); 
             setSessionExpired(true); 
         } else if (!request.ok && request.error === 'NotFound') {
             setSearchResults({searchResults: []}); 
+            setErrorMessage(''); 
         } else {
             setSearchResults(undefined); 
+            setErrorMessage('Failed to search, please try again later.'); 
+        }
+    }
+
+    async function handleCardClick(cardDetails: {
+        boardId: number;
+        cardId: number;
+        listId: number;
+        cardName: string;
+        boardName: string;
+        listName: string;
+    }) {
+        if (boardDetails && cardDetails.boardId === boardDetails.id) {
+            setSearchInput(''); 
+            setSearchResults(undefined); 
+            setErrorMessage(''); 
+            setCardDetailsPanelData({
+                parentListId: `list-${cardDetails.listId}`, 
+                cardId: `card-${cardDetails.cardId}`
+            }); 
+            setActivePanel('cardDetailsPanel'); 
+        } else {
+            try {
+                setBoardLoading(true); 
+                const boardDataRequest = await ApiRequestWithRefreshTokenAttemptAndData(GetBoardRequest, cardDetails.boardId); 
+
+                if (boardDataRequest.ok && boardDataRequest.data !== undefined) {
+                    hydrateBoard(NormaliseBoardData(boardDataRequest.data)); 
+                    setSearchInput(''); 
+                    setSearchResults(undefined); 
+                    setErrorMessage(''); 
+                    setCardDetailsPanelData({
+                        parentListId: `list-${cardDetails.listId}`, 
+                        cardId: `card-${cardDetails.cardId}`
+                    }); 
+                    setActivePanel('cardDetailsPanel'); 
+                    setLastUsedBoard(boardDataRequest.data.boardId); 
+                } else if (!boardDataRequest.ok && boardDataRequest.error === 'Unauthorized') {
+                    setSessionExpired(true); 
+                } else {
+                    setErrorMessage('Failed to load board data, please try again later.'); 
+                }
+
+            } finally {
+                setBoardLoading(false); 
+            }
+        }
+    }
+
+    async function setLastUsedBoard(boardId: number) {
+        const lastUsedBoardResult =  await ApiRequestWithRefreshTokenAttemptAndData(
+            UpdateLastUsedBoardRequest, boardId); 
+
+        if (lastUsedBoardResult.ok) {
+            setLastUsedBoardExists(true); 
+        } else if (!lastUsedBoardResult.ok && lastUsedBoardResult.error === 'Unauthorized') {
+            setSessionExpired(true); 
         }
     }
 
@@ -111,6 +179,7 @@ export default function DashboardSearchButton() {
                             searchInput.length > 0 ? 
                             <div className={styles.resultsWrapper}>
                                 <header>Search results</header>
+                                <div className={styles.error}>{errorMessage}</div>
                                 {
                                     loading ?
                                     <div className={styles.loading}>
@@ -119,17 +188,16 @@ export default function DashboardSearchButton() {
                                     : 
                                     <div className={styles.results}>
                                         {
-                                            searchResults === undefined ? 
-                                            <div className={styles.error}>Failed to search, please try again later.</div>
+                                            searchResults === undefined ? null
                                             :
                                             searchResults.searchResults.length === 0 ? 
                                             <div className={styles.cardNotFound}>We couldn't find anything matching your search.</div>
                                             : 
                                             <div className={styles.resultList}>
-                                                {searchResults.searchResults.map((r) => 
-                                                    <div key={r.cardId}>
-                                                        <span className={styles.cardName}>{r.cardName}</span>
-                                                        <span className={styles.cardInfo}>{r.boardName}: {r.listName}</span>
+                                                {searchResults.searchResults.map((cardDetails) => 
+                                                    <div key={cardDetails.cardId} onClick={() => handleCardClick(cardDetails)}>
+                                                        <span className={styles.cardName}>{cardDetails.cardName}</span>
+                                                        <span className={styles.cardInfo}>{cardDetails.boardName}: {cardDetails.listName}</span>
                                                     </div>
                                                 )}
                                             </div>
