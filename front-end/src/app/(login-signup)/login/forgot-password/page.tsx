5f5beb703b9147476214914de77043ce2f95849f
Maninder Singh <maninder123450@gmail.com>
2026-05-06 17:55:09 +0100

    Mock pages for forgot password and reset password pages

---
+++ front-end/src/app/(login-signup)/login/forgot-password/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+
+export default function ForgotPassword() {
+
+    return (
+        <div>
+            <h1>Forgot password</h1>
+            <p>Please enter your email</p>
+            <input />
+        </div>
+    ); 
+}
---
+++ front-end/src/app/(login-signup)/reset-password/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function ResetPassword() {
+   
+    return (
+        <div>
+            <h1>Reset password</h1>
+            <p>Please enter new password</p>
+        </div>
+    ); 
+}
