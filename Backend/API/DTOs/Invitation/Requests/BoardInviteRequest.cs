0016e2c283fa2881806ded3c45bad3b4be2bdc1e
Maninder Singh <maninder123450@gmail.com>
2026-05-08 18:06:17 +0100

    Added initial files for invitation controller

---
+++ Backend/API/DTOs/Invitation/Requests/BoardInviteRequest.cs
@@ -0,0 +1,17 @@
+﻿using DatabaseContext.Types;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs.Invitation.Requests; 
+
+public class BoardInviteRequest
+{
+    [Required]
+    public int BoardId { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public required string InvitedUserEmail { get; set; }
+
+    [Required]
+    public required Role Role { get; set; }
+}
