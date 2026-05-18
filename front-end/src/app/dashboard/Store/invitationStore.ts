3f2d28bd03ac40d8338e7e283a8c6133ef2c7c1e
Maninder Singh <maninder123450@gmail.com>
2026-05-18 15:55:08 +0100

    Added function to set invitations

--- front-end/src/app/dashboard/Store/invitationStore.ts
+++ front-end/src/app/dashboard/Store/invitationStore.ts
@@ -7,12 +7,16 @@ type State = {
 }
 
 type Action = {
+    setInvitations: (invitations: InvitationsInfo) => void; 
+
     acceptInvitation: (id: number) => void; 
     rejectInvitation: (id: number) => void; 
 }
 
 export const useInvitationStore = create<State & Action>((set) => ({
     invitations: [], 
+
+    setInvitations: (invitations) => set({invitations: invitations }),
 
     acceptInvitation: (id) => set((state) => ({
         invitations: state.invitations.map(invitation => {
