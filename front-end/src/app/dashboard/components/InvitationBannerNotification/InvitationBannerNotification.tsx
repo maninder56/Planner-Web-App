2934ab2db6ca6b36ac465cea2950f1ec7d648be2
Maninder Singh <maninder123450@gmail.com>
2026-05-22 12:41:19 +0100

    Improved animation and added red dot to indicate pending message

--- front-end/src/app/dashboard/components/InvitationBannerNotification/InvitationBannerNotification.tsx
+++ front-end/src/app/dashboard/components/InvitationBannerNotification/InvitationBannerNotification.tsx
@@ -31,6 +31,7 @@ export default function InvitationBannerNotification() {
             <button className={styles.inboxButton} onClick={e => {
                 e.stopPropagation(); 
                 setActivePanel('inboxOptionsPanel'); 
+                handleCloseClick(); 
             }}>
                 <svg
                     width="20"
@@ -53,6 +54,15 @@ export default function InvitationBannerNotification() {
                         <path
                         d="M54.5,51.1L20,120L20,180C20,190.972 29.028,200 40,200L200,200C210.972,200 220,190.972 220,180L220,120L185.5,51.1C182.128,44.314 175.178,40.004 167.6,40L72.4,40C64.822,40.004 57.872,44.314 54.5,51.1"
                         className={styles.inboxLine}
+                        />
+                    </g>
+                    <g transform="matrix(0.87127,0,0,0.867115,10.218253,-0.671147)">
+                        <ellipse
+                            cx="6.636"
+                            cy="5.387"
+                            rx="4.591"
+                            ry="4.613"
+                            className={styles.inboxDot}
                         />
                     </g>
                     </svg>
