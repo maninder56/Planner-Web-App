b278df3766e1aeb7b656cdbb30128cede2780e55
Maninder Singh <maninder123450@gmail.com>
2026-05-25 17:52:59 +0100

    Added retry attepts logic

--- front-end/src/app/dashboard/Services/signalRInvitationService.ts
+++ front-end/src/app/dashboard/Services/signalRInvitationService.ts
@@ -1,5 +1,7 @@
 'use client'; 
 
+import { RefreshTokensRequest } from '@/Services/ApiRequest';
+import { useUserStore } from '@/Store/userStore';
 import {
   HubConnection,
   HubConnectionBuilder,
@@ -7,6 +9,7 @@ import {
 } from '@microsoft/signalr';
 
 const apiUrl = process.env.NEXT_PUBLIC_APIURL; 
+
 
 class SignalRInvitationService {
     public connection: HubConnection; 
@@ -19,13 +22,36 @@ class SignalRInvitationService {
     }
 
     async start() {
-        try {
-            if (this.connection.state === HubConnectionState.Disconnected) {
-                await this.connection.start(); 
-                console.log('Signal R connected'); 
-            } 
-        } catch(error) {
-            console.error('SignalR start failed', error); 
+        const delayMs = 2000; 
+        const maxAttempts = 2; 
+        let attempts = 0; 
+        
+        while(attempts < maxAttempts) {
+            if (useUserStore.getState().sessionExpired) {
+                console.warn('Session expired, aborting SignalR connection.');
+                return;
+            }
+
+            try {
+                if (this.connection.state === HubConnectionState.Disconnected) {
+                    await this.connection.start(); 
+                    console.log('Signal R connected'); 
+                    return; 
+                } 
+                return; 
+            } catch(error) {
+                attempts++;
+                
+                console.error(`SignalR start failed (attempt ${attempts})`, error);
+              
+                if (attempts < maxAttempts) {
+                    await new Promise(resolve => setTimeout(resolve, delayMs));
+                }
+            }
+        }
+
+        if (attempts < maxAttempts) {
+            console.error("Max connection retry attempts reached. Giving up.");
         }
     }
 }
