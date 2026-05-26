f5c14ab07928327d89ad50fe0439cb3234681959
Maninder Singh <maninder123450@gmail.com>
2026-05-26 10:20:39 +0100

    added refresh tokens function and loading state

--- front-end/src/app/dashboard/page.tsx
+++ front-end/src/app/dashboard/page.tsx
@@ -11,10 +11,19 @@ import SignalRInvitationProvider from './providers/signalRInvitationProvider';
 import { useInvitationStore } from './Store/invitationStore';
 import InvitationBannerNotification from './components/InvitationBannerNotification/InvitationBannerNotification';
 import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
+import BoardHeaderSkeleton from './components/Board/BoardHeader/BoardHeaderSkeleton/boardHeaderSkeleton';
+import BoardContentLoadingSkeleton from './components/Board/BoardContent/BoardContentLoadingSkeleton/boardContentLoadingSkeleton';
+import DashboardHeaderLoadingSkeleton from './components/DashboardHeader/DashboardHeaderLoadingSkeleton/dashboardHeaderLoadingSkeleton';
+import { RefreshTokensRequest } from '@/Services/ApiRequest';
+import DashboardErrorPage from './components/DashboardError/dashboardErrorPage';
 
 export default function Dashboard() {
     const setActivePanel = useBoardUIStore((state) => state.setActivePanel); 
     const isSessionExpired = useUserStore((state) => state.sessionExpired);  
+    const setSessionExpired = useUserStore((state) => state.setSessionExpired); 
+    const [loading, setLoading] = useState(true); 
+    const [showErrorPage, setShowErrorPage] = useState(false); 
 
     // signalR library uses require which turbopack can't statically analyze. 
     // use of dynamic is to make sure the module is rendered only on client side
@@ -22,6 +31,52 @@ export default function Dashboard() {
         () => import('./providers/signalRInvitationProvider'),
         { ssr: false }
     );
+
+    async function fetchRefrehTokens() {
+        const request = await RefreshTokensRequest(); 
+        setSessionExpired(false); 
+
+        if (request.ok) {
+            setLoading(false); 
+        } else if (request.error === 'BadRequest') {
+            setSessionExpired(true); 
+        } else {
+            setLoading(false); 
+            setShowErrorPage(true); 
+        }
+    }
+
+    useEffect(() => {
+        fetchRefrehTokens(); 
+    }, []); 
+
+    function handleTryAgain() {
+        setLoading(true); 
+        setShowErrorPage(false); 
+        fetchRefrehTokens(); 
+    }
+
+
+    if (loading) {
+        return (
+            <div className={styles.page}
+                onClick={(e) => {
+                    e.stopPropagation(); 
+                    setActivePanel('none'); 
+                }}>
+                    <DashboardHeaderLoadingSkeleton />
+                    <BoardHeaderSkeleton />
+                    <BoardContentLoadingSkeleton />
+                    { isSessionExpired && <SessionExpired /> }
+            </div>
+        ); 
+    }
+
+    if (showErrorPage) {
+        return (
+            <DashboardErrorPage onTryAgainClick={handleTryAgain}/>
+        );
+    }
 
     return (
         <SignalRInvitationProvider>
