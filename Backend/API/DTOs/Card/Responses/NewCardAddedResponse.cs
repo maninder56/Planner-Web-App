6eef1b44e5e4d865756f219be1f226f66ddc26a1
Maninder Singh <maninder123450@gmail.com>
2026-06-04 14:58:07 +0100

    DTOs for new list, new card and updated list name

---
+++ Backend/API/DTOs/Card/Responses/NewCardAddedResponse.cs
@@ -0,0 +1,24 @@
+﻿using DatabaseContext.Types;
+
+namespace API.DTOs.Card.Responses; 
+
+public class NewCardAddedResponse
+{
+    public required int ByUserId { get; set; }
+
+    public int CardId { get; set; }
+
+    public required string Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public int CardPosition { get; set; }
+
+    public bool IsDone { get; set; }
+
+    public DateOnly DueDate { get; set; }
+
+    public Priority Priority { get; set; }
+
+    public int BoardListId { get; set; }
+}
---
+++ Backend/API/DTOs/List/Responses/ListNameUpdated.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DTOs.List.Responses; 
+
+public class ListNameUpdated
+{
+    public required int ByUserId { get; set; }
+
+    public required string NewName { get; set; }
+
+}
