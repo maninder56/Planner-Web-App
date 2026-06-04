9591ded6b05e29ccbf77fa969e93ad40df9eb4a2
Maninder Singh <maninder123450@gmail.com>
2026-06-04 17:45:52 +0100

    Added card updated DTO

---
+++ Backend/API/DTOs/Card/Responses/CardUpdatedResponse.cs
@@ -0,0 +1,22 @@
+﻿using DatabaseContext.Types;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs.Card.Responses; 
+
+public class CardUpdatedResponse
+{
+    public required int ByUserId { get; set; }
+    public required int ListId { get; set; }
+    public required int CardId { get; set; }
+    public required int BoardId { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public bool? IsDone { get; set; }
+
+    public DateOnly? DueDate { get; set; }
+
+    public Priority? Priority { get; set; }
+}
