a4bd58ff4acb40da5b7de65aa52d1b21147ec847
Maninder Singh <maninder123450@gmail.com>
2026-06-07 14:00:10 +0100

    Fixed bug; list name remained even after form was closed

--- front-end/src/app/dashboard/components/Board/BoardContent/AddNewListButton/addNewListButton.tsx
+++ front-end/src/app/dashboard/components/Board/BoardContent/AddNewListButton/addNewListButton.tsx
@@ -7,6 +7,7 @@ import { CreateNewListRequest } from '@/app/dashboard/Services/listService';
 import { useUserStore } from '@/Store/userStore';
 import { useBoardStore } from '@/app/dashboard/Store/boardStore';
 import { UserRole } from '@/app/dashboard/Types/boardTypes';
+import AddNewListOptions from './addNewListOptions';
 
 export default function AddNewListButton({
     boardId, 
@@ -17,36 +18,8 @@ export default function AddNewListButton({
 }) {
     const isFormOpen = useBoardUIStore((state) => state.activePanel === 'newListForm'); 
     const setActivePanel = useBoardUIStore((state) => state.setActivePanel); 
-    const setSessionExpired = useUserStore((state) => state.setSessionExpired); 
-    const addNewListToBoard = useBoardStore((state) => state.AddNewListToBoard); 
-
-    const [listName, setListName] = useState(''); 
-    const [error, setError] = useState(''); 
-    const [buttonDisabled, setButtonDisabled] = useState(false); 
 
     const viewOnly = userRole === 'Viewer'; 
-
-    async function handleSubmit(e: FormEvent) {
-        e.preventDefault(); 
-        setButtonDisabled(true); 
-
-        try {
-            const request = await ApiRequestWithRefreshTokenAttemptAndData(CreateNewListRequest, { boardId: boardId, newListName: listName, }); 
-
-            if (request.ok && request.data !== undefined) {
-                addNewListToBoard({id: request.data.id, title: request.data.name, position: request.data.listPosition}); 
-                setListName(''); 
-                setError(''); 
-                setActivePanel('none'); 
-            } else if (!request.ok && request.error === 'Unauthorized') {
-                setSessionExpired(true); 
-            } else {
-                setError('Failed to create new List, please try again.'); 
-            }
-        } finally {
-            setButtonDisabled(false); 
-        }
-    }
 
     if (viewOnly) {
         return null; 
@@ -64,31 +37,7 @@ export default function AddNewListButton({
                 </svg>
                 <span>Add New List</span>
             </button>
-            { 
-                isFormOpen &&
-                <div className={styles.fromContainer} onClick={(e) => {e.stopPropagation();}}>
-                    <form onSubmit={handleSubmit}>
-                        <div className={styles.inputContainer}>
-                            <input
-                                name='newListName'
-                                type='text'
-                                placeholder='Enter list name...'
-                                maxLength={30}
-                                value={listName}
-                                autoFocus={true}
-                                onChange={(e) => setListName(e.target.value)} />
-                        </div>
-                        <span className={styles.error}>{error}</span>
-                        <div className={styles.listFromButtons}>
-                            <button className='button transparent-with-outline' type='button' disabled={buttonDisabled} onClick={(e) => {
-                                e.stopPropagation(); 
-                                setActivePanel('none'); 
-                            }}>Cancel</button>
-                            <button type='submit' className='button blue' disabled={listName === '' || buttonDisabled}>Add List</button>
-                        </div>
-                    </form>
-                </div>
-            }
+            { isFormOpen && <AddNewListOptions boardId={boardId} /> }
         </div>
     ); 
 }
