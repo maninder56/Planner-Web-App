1d966888c3f8c2f0f1b7e5a527ad2ee7818c91ab
Maninder Singh <maninder123450@gmail.com>
2026-07-10 10:05:35 +0100

    Improved delete query to also delete boards related to user

--- Backend/API/Repositories/Profile/ProfileRepository.cs
+++ Backend/API/Repositories/Profile/ProfileRepository.cs
@@ -1,6 +1,7 @@
 ﻿using API.Exceptions;
 using DatabaseContext;
 using Microsoft.EntityFrameworkCore;
+using DatabaseContext.Types; 
 
 namespace API.Repositories.Profile; 
 
@@ -20,7 +21,16 @@ public class ProfileRepository(PlannerContext database) : IProfileRepository
 
     public async Task DeleteProfileAsync(int userId)
     {
+        await using var transaction = await database.Database.BeginTransactionAsync();
+
+        await database.Boards
+            .Where(b => b.BoardMembers
+                .Any(bm => bm.UserId == userId && bm.Role == Role.Owner))
+            .ExecuteDeleteAsync(); 
+
         await database.Users.Where(u => u.UserId == userId)
             .ExecuteDeleteAsync(); 
+
+        await transaction.CommitAsync();
     }
 }
